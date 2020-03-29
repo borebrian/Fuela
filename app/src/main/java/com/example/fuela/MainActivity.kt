157c60a2168b166cb9097beb22966505f038d959
K-HUB  LATITUDE <borebrian77@gmai.com>
2020-03-29 01:12:18 -0700

    v2

--- app/src/main/java/com/example/fuela/MainActivity.kt
+++ app/src/main/java/com/example/fuela/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.fuela
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
